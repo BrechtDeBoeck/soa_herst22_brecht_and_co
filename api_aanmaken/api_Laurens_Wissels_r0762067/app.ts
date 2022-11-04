849df04ae8f1e9683365dc2b2b13e4ae3721feb6
r0762067 <laurens.wissels@student.ucll.be>
2022-11-04 14:04:46 +0000

    setup

--- api_aanmaken/api_Laurens_Wissels_r0762067/app.ts
+++ api_aanmaken/api_Laurens_Wissels_r0762067/app.ts
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 
 const path = require('path'); 
-require('dotenv').config({ path: path.join(__dirname, '.env') });
+require('dotenv').config({path: path.join(__dirname, '.env') });
 console.log(process.env)
 
 app.use('/order', require('./domain/routes/order_route'));
 app.use('/book', require('./domain/routes/book_route'));
 
-const PORT = 3000
-const ENV = "testing"
+const PORT = process.env.PORT
 const DATABASE = "foo"
-app.listen(PORT, console.log(`Server has started:\n -env:${ENV}\n -port:${PORT}\n -db:${DATABASE}\n`))
+app.listen(PORT, console.log(`Server has started:\n -port:${PORT}\n -db:${DATABASE}\n`))
